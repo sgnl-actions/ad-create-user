b84074dac5e8719c878762116876096ffbde513c
Dhanya H <117677327+dhanya-sgnl@users.noreply.github.com>
2026-03-19 11:53:49 -0700

    Revert "[sc-75075] Re-bundle ldapts"

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,5 @@
 import { createConfig } from '@sgnl-actions/rollup-config';
-import json from '@rollup/plugin-json';
 
 export default createConfig({
-  inlineDynamicImports: true,
-  plugins: [json()]
+  external: ['ldapts']
 });
