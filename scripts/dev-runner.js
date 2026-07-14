e5d7051615d10057f48e6639416e6b82e94bb8f7
Richard Gao <richard.gao@crowdstrike.com>
2026-07-14 15:57:02 -0600

    Fix lint

--- scripts/dev-runner.js
+++ scripts/dev-runner.js
@@ -14,7 +14,7 @@ import script from '../src/script.mjs';
 // Read configuration from environment variables (set in .env)
 const mockContext = {
   environment: {
-    ADDRESS: process.env.ADDRESS || 'ldap://localhost:389',
+    ADDRESS: process.env.ADDRESS || 'ldap://localhost:389'
   },
   secrets: {
     BASIC_USERNAME: process.env.BASIC_USERNAME || '',
@@ -33,7 +33,7 @@ const mockParams = {
   // password: 'SecurePassword123!',
   enabled: process.env.ENABLED === 'true',
   successIfAlreadyExists: process.env.SUCCESS_IF_ALREADY_EXISTS !== 'false',
-  dry_run: process.env.DRY_RUN === 'true'
+  dry_run: process.env.DRY_RUN === 'true',
   tlsSkipVerify: process.env.TLS_SKIP_VERIFY === 'true'
 };
 
